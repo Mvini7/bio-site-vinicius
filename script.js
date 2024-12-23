69b8b8a65b8514320369ff5872bc13f6e6b28c9b
VINICIUS RODRIGUES <158781792+Mvini7@users.noreply.github.com>
2024-12-23 12:32:26 -0300

    finalizado

---
+++ script.js
@@ -0,0 +1,17 @@
+const instagram = document.getElementById('instagram')
+
+instagram.addEventListener('click', function() {
+    window.location.href = 'https://instagram.com/viniciuswqi'
+})
+
+const linkedin = document.getElementById('linkedin')
+
+linkedin.addEventListener('click', function() {
+    window.location.href = 'https://linkedin.com/in/vinicius-rodriguesdev'
+})
+
+const portfolio = document.getElementById('portfolio')
+
+portfolio.addEventListener('click', function() {
+    window.location.href = 'https://portfolio-vr-dev.vercel.app/'
+})
